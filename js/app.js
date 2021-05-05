b517b97f931ab6592d8a8c8be61242925ef85a22
Reza Esmaili <esmailireza20@gmail.com>
2021-05-06 00:14:03 +0430

    edit some pages

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click",function(){
-    if(links.classList.contains("show-links")){
-        links.classList.remove("show-links");
-    }
-    else{
-        links.classList.add("show-links");
-    }
+// menu hamberger mobile
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
 });
 
 // go to top (button)
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function(){ scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
-function scrollFunction(){
-    if(document.body.scrollTop > 850 || document.documentElement.scrollTop > 850){
-        mybutton.style.display = "block";
-    }
-    else{
-        mybutton.style.display = "none";
-    }
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 850 ||
+    document.documentElement.scrollTop > 850
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-function topFunction(){
-    document.body.scrollTop = 0 ; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
